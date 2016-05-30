79056ac6027bbf26e0977b23e0b2bce9005850d3
binhqd <binhqd@gmail.com>
2016-05-31 00:59:06 +0700

    feat(report): Export HTML report
    Enable create CHANGELOG.html by adding -r parameter to the CLI
    useage: clgt -t <tag> -u <url> -r html

--- index.js
+++ index.js
@@ -6,6 +6,9 @@
   var generator = require('./lib/generate');
   var nconf = require('nconf');
   var log = require('./lib/log');
+  var fs = require('fs');
+  var marked = require('marked');
+  var File = require('./lib/file')
 
   if (!module.parent) {
     var file = _.cloneDeep(nconf.file('.clgtrc').stores.file.store);
@@ -20,15 +23,27 @@
       return;
     }
 
-    var argv = _.pick(args.stores.argv.store, [ 'description', 'file', 'name', 'tag', 'url' ]);
-
+    var argv = _.pick(args.stores.argv.store, [ 'description', 'file', 'name', 'tag', 'url', 'report' ]);
     var options = _.defaultsDeep(argv, file);
-    generator.generate(options, function(err) {
 
+    generator.generate(options, function(err) {
       if (err) {
         log('error', err);
       } else {
         log('success', 'Done generating to', options.file);
+        if (options.report && options.report === 'html') {
+          fs.readFile(options.file, 'utf8', function(err, contents) {
+            if (err) {
+              log('error', err);
+            }
+
+            // TODO: Need to change the default html report file name
+            var file = new File("CHANGELOG.html");
+            file.write(marked(contents));
+            file.end();
+            log('success', 'and also CHANGELOG.html');
+          });
+        }
       }
     });
   }
